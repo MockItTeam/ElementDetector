a43fac678f6a9572a6702db93552f3fe0319908f
mapfap <mapfaps@gmail.com>
2016-02-20 00:52:39 +0700

    Draw vertex's order

--- contour.py
+++ contour.py
@@ -24,6 +24,7 @@ def draw(img, vertices, color):
   vertex_count = len(vertices)
   for i in range(vertex_count - 1):
     cv2.circle(img, vertices[i].tuple(), 2, color, -1)
+    cv2.putText(img, str(i), vertices[i].tuple(), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
     cv2.line(img, vertices[i].tuple(), vertices[i + 1].tuple(), color, 1)
   cv2.line(img, vertices[vertex_count - 1].tuple(), vertices[0].tuple(), color, 1) # Draw line from last vertex to the first one.
 
@@ -34,19 +35,19 @@ def rand_color():
   return r, g, b
 
 def main(argv):
-  img = cv2.imread('/Users/mapfap/Desktop/test3.jpg')
-  img = cv2.resize(img, (int(img.shape[1] * 0.5), int(img.shape[0] * 0.5)))
+  img = cv2.imread('/Users/mapfap/Desktop/test.jpg')
+  img = cv2.resize(img, (int(img.shape[1] * 1), int(img.shape[0] * 1)))
   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-  # kernel = np.ones((2, 2), np.uint8)
-  # erosion = cv2.erode(gray, kernel, iterations = 1)
-  erosion = gray # Skip erosion
+  kernel = np.ones((2, 2), np.uint8)
+  erosion = cv2.erode(gray, kernel, iterations = 1)
+  # erosion = gray # Skip erosion
 
   height, width = gray.shape
   newimg = np.zeros((height, width, 3), np.uint8)
 
   # Canny edge detector
-  thresh = 0
+  thresh = 180
   edges = cv2.Canny(erosion, thresh, thresh * 2)
 
   contours,hierarchy = cv2.findContours(edges, cv2.cv.CV_RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
@@ -57,8 +58,33 @@ def main(argv):
       approx = cv2.approxPolyDP(cnt, 0.015 * cv2.arcLength(cnt, True), True)
       vertices = get_vertices(approx)
       area = polygon_area(vertices)
-      print "area=%d, vertices=%d" % (area, len(vertices))
-      draw(newimg, vertices, rand_color())
+      vertex_count = len(vertices)
+      # print "area=%d, vertices=%d" % (area, len(vertices))
+      will_draw = False
+      if (vertex_count == 1):
+        pass
+      elif (vertex_count == 2):
+        pass
+      elif (vertex_count == 3):
+        pass
+      elif (vertex_count == 4):
+        # if area > 100:
+        will_draw = True
+        pass
+      elif (vertex_count == 5):
+        pass
+      elif (vertex_count == 6):
+        pass
+      elif (vertex_count == 7):
+        pass
+      elif (vertex_count == 8):
+        # will_draw = True
+        pass
+      else:
+        pass
+        
+      if (will_draw):
+        draw(newimg, vertices, rand_color())
 
   cv2.imshow('Show', newimg)
   cv2.waitKey(0)
