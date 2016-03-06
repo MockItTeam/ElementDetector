da7aa9c0aa66af3ccf1be4eb34a372a8d33fe2fa
mapfap <mapfaps@gmail.com>
2016-03-06 12:30:37 +0700

    Change naming context

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys
 from PyQt4 import QtGui
 from gui import ImageDebuggerGUI
+from processor import ElementDetector
 
 if __name__ == "__main__":
-  app = QtGui.QApplication(sys.argv)
-  ui = ImageDebuggerGUI()
-  ui.show()
-  sys.exit(app.exec_())
+  # app = QtGui.QApplication(sys.argv)
+  # ui = ImageDebuggerGUI()
+  # ui.show()
+  # sys.exit(app.exec_())
+  detector = ElementDetector()
+  detector.detect("img/test10.jpg")
